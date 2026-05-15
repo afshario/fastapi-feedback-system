b69b632d2ff91f7a53d99c229690d7112a903637
MHAfshari <mhafshari1385@gmail.com>
2026-05-15 23:00:04 +0330

    created User model

---
+++ src/models/users.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
+from sqlalchemy.sql import func
+from src.db import Base
+
+class Users(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    is_admin = Column(Boolean , default= False)
+    is_active = Column(Boolean , default= False)
+    password = Column(String(255), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+
+class AdminProfiles(Base):
+    __tablename__ = "admin_profiles"
+    
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"), unique=True)
+    Field = Column(String(500))
